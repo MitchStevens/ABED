1983b1e38b4307cadcbacfd4f4e7eb9c6f434ce1
Mitch Stevens <MitchStevens@users.noreply.github.com>
2015-05-27 16:51:10 +1000

    Update Operation.java

--- src/logic/Operation.java
+++ src/logic/Operation.java
@@ -1,12 +1,17 @@
+package logic;
+
+import java.util.List;
+import java.util.Stack;
+
 public class Operation extends Token{
 	String op;
 	String name;
 	int precedence;
 	int args;
 	
-  public static Operation and = new Operation("and", "&", 2);
-  public static Operation or  = new Operation("or",  "|", 2);
-  public static Operation not = new Operation("not", "~", 1)
+  public static Operation and = new Operation("and", "&", 2, 2);
+  public static Operation or  = new Operation("or",  "|", 2, 2);
+  public static Operation not = new Operation("not", "~", 1, 1);
   
   public Operation(String name, String op, int p, int args){
     this.name = name;
@@ -15,11 +20,12 @@ public class Operation extends Token{
     this.args = args;
   }
   
-  public Boolean apply(List<Boolean> bools){
+
+  public Input apply(Stack<Token> bools){
 		switch(name){
-		case "and": return new Input( bools.get(0).get() && bools.get(1).get() );
-		case "or":  return new Input( bools.get(0).get() || bools.get(1).get() );
-		case "not": return new Input( !bools.get(0).get() );
+		case "and": return new Input( ((Input)bools.pop()).b && ((Input)bools.pop()).b );
+		case "or":  return new Input( ((Input)bools.pop()).b || ((Input)bools.pop()).b );
+		case "not": return new Input( !((Input)bools.pop()).b );
 		}
 		throw new Error("Operator "+op+" not found!");
 	}
